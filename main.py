f2f45841f706234af72130fc99bed42e5a6c1b48
Connok <mccarthyrcconnor@gmail.com>
2024-10-21 20:09:48 -0400

    Game finished bassic. need to add features

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
+import sys
 import pygame
 from constants import *
 from player import Player
 from asteroid import Asteroid
-from asteroidfeild import AsteroidField
+from asteroidfield import AsteroidField
+from shot import Shot
 
 
 def main():
-    pygame.init
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
 
-    updateable = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
-    Asteroid.containers = (asteroids, updateable, drawable)
-    AsteroidField.containers = updateable
-    asteroid_feild = AsteroidField()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
 
-    Player.containers = (updateable, drawable)
+    Player.containers = (updatable, drawable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
@@ -29,9 +33,19 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        for obj in updateable:
+        for obj in updatable:
             obj.update(dt)
-        
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    shot.kill()
+                    asteroid.split()
+
         screen.fill("black")
 
         for obj in drawable:
@@ -45,4 +59,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
